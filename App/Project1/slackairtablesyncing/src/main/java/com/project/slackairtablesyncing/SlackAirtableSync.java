1ed8b9dd0d2188c71c4f669b3e53bf3cca560cd8
poromin <poromin93@gmail.com>
2023-06-12 16:04:44 +0700

    update slack fetching

--- App/Project1/slackairtablesyncing/src/main/java/com/project/slackairtablesyncing/SlackAirtableSync.java
+++ App/Project1/slackairtablesyncing/src/main/java/com/project/slackairtablesyncing/SlackAirtableSync.java
@@ -19,62 +19,11 @@ public class SlackAirtableSync {
     private static final String AIRTABLE_TABLE_NAME = "Slack";
 
     public static void main(String[] args) {
-        Slack slack = Slack.getInstance();
 
         // Sync Slack users to Airtable
-        syncSlackUsersToAirtable(slack);
+//        syncSlackUsersToAirtable(slack);
 
         // Sync Slack channels to Airtable
-        syncSlackChannelsToAirtable(slack);
-    }
-
-    private static void syncSlackUsersToAirtable(Slack slack) {
-        UsersListResponse response = slack.methods().usersList(req -> req.token(SLACK_TOKEN));
-
-        if (response.isOk()) {
-            List<User> users = response.getMembers();
-
-            Airtable airtable = new Airtable(AIRTABLE_API_KEY);
-
-            for (User user : users) {
-                String userId = user.getId();
-                String userName = user.getName();
-
-                try {
-                    Table table = airtable.base(AIRTABLE_BASE_ID).table(AIRTABLE_TABLE_NAME);
-                    table.create(new TextField("User ID", userId), new TextField("Username", userName));
-                    System.out.println("Synced user to Airtable: " + userName);
-                } catch (AirtableApiException e) {
-                    System.out.println("Failed to sync user to Airtable: " + e.getMessage());
-                }
-            }
-        } else {
-            System.out.println("Failed to fetch Slack users: " + response.getError());
-        }
-    }
-
-    private static void syncSlackChannelsToAirtable(Slack slack) {
-        ChannelsListResponse response = slack.methods().channelsList(req -> req.token(SLACK_TOKEN));
-
-        if (response.isOk()) {
-            List<Channel> channels = response.getChannels();
-
-            Airtable airtable = new Airtable(AIRTABLE_API_KEY);
-
-            for (Channel channel : channels) {
-                String channelId = channel.getId();
-                String channelName = channel.getName();
-
-                try {
-                    Table table = airtable.base(AIRTABLE_BASE_ID).table(AIRTABLE_TABLE_NAME);
-                    table.create(new TextField("Channel ID", channelId), new TextField("Channel Name", channelName));
-                    System.out.println("Synced channel to Airtable: " + channelName);
-                } catch (AirtableApiException e) {
-                    System.out.println("Failed to sync channel to Airtable: " + e.getMessage());
-                }
-            }
-        } else {
-            System.out.println("Failed to fetch Slack channels: " + response.getError());
-        }
+//        syncSlackChannelsToAirtable(slack);
     }
 }
