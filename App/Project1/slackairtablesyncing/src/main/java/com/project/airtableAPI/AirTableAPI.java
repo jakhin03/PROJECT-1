dbd5537a7f95fa9bf1dc3ac53a086db1cf8bbe99
Khanh Cao <caokhanh03.ck@gmail.com>
2023-06-16 00:28:16 +0700

    update api and readme

---
+++ App/Project1/slackairtablesyncing/src/main/java/com/project/airtableAPI/AirTableAPI.java
@@ -0,0 +1,10 @@
+package com.project.airtableAPI;
+
+public class AirTableAPI {
+	private String url = "https://api.airtable.com/v0/"
+	private String path;
+	private String header = "Authorization"
+	public void getRecord() {
+		
+	}
+}
