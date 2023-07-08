cebe8c06599c391180cc9268b7b6b54f8a761ba3
poromin <poromin93@gmail.com>
2023-07-09 00:35:46 +0700

    Update SlackDataFetching.java

--- App/SlackAirtableSyncing/src/com/project/slackdatafetching/SlackDataFetching.java
+++ App/SlackAirtableSyncing/src/com/project/slackdatafetching/SlackDataFetching.java
@@ -28,9 +28,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SlackDataFetching {
 	
